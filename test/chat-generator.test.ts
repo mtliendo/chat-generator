3934932a66881e87c09ee1a79657a3c360fffff3
focusotter <mtliendo@focusotter.com>
2023-05-22 02:54:42 -0500

    Initial commit

---
+++ test/chat-generator.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ChatGenerator from '../lib/chat-generator-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/chat-generator-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ChatGenerator.ChatGeneratorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
